8e9564c4d3b3f784babe425334c56329701a6f0a
C4rTank <hassamsdss@gmail.com>
2024-05-01 13:20:27 -0700

    020 Functions

--- task1.py
+++ task1.py
@@ -1,6 +1,28 @@
+'''
+Task 1:
+Create a program with 3 function definitions:
+function A prints the message "Hello"
+function B prints the message "How are you"
+function C prints the message "Hi"
+
+Ask the user to enter a letter from A to C
+Execute the function of the letter they use.
+'''
+
+
+
 def A():
-
+    print("Hello")
 def B():
-
-
+    print("How are you")
 def C():
+    print("Hi")
+
+picked = input("Please choose a letter from A to C: ")
+
+if picked == 'A':
+        A()
+if picked == 'B':
+        B()
+if picked == 'C':
+        C()
--- task2.py
+++ task2.py
@@ -8,3 +8,24 @@ displays instructions and how to play
 game()
 plays the games
 """
+
+
+
+def title():
+    import random
+    
+    print("Lets play a number gussing game!")
+    print("Guess the number I'm thinking of between 1 to 10!")
+
+    num = random.randint(1, 10)
+
+    guess = int(input("Guess what number I'm thinking of: "))
+
+    if guess == num:
+        print("Great job!")
+    else:
+        print("Wrong!")
+        print("The number was",num, end='')
+        print('!')
+
+title()
